2230d2cfb89b7bdd8905ab855c866900dd3205f5
Riley Lum <rileylum@adam.com.au>
2024-02-01 15:26:32 +1030

    use TreeWalker to traverse DOM instead

--- content.js
+++ content.js
@@ -1,13 +1,22 @@
-const text = document.querySelectorAll('h1,h2,h3,h4,h5,p,li,td,caption,span,a,div')
 
-for (let i = 0; i < text.length; i++) {
-    if (text[i].innerHTML.toLowerCase().includes('xandering')) {
-        text[i].innerHTML = text[i].innerHTML.replace(/xandering/gi, 'Jaquaysing')
+var walker = document.createTreeWalker(
+    document.body, 
+    NodeFilter.SHOW_TEXT, 
+    null, 
+    false
+);
+
+var node;
+
+while (node = walker.nextNode()) {
+    if (node.nodeValue.toLowerCase().includes('xandering')) {
+        node.nodeValue = node.nodeValue.replace(/xandering/gi, 'Jaquaysing')
     }
-    if (text[i].innerHTML.toLowerCase().includes('jaquaying')) {
-        text[i].innerHTML = text[i].innerHTML.replace(/jaquaying/gi, 'Jaquaysing')
+    
+    if (node.nodeValue.toLowerCase().includes('jaquaying')) {
+        node.nodeValue = node.nodeValue.replace(/jaquaying/gi, 'Jaquaysing')
     }
-    if (text[i].innerHTML.toLowerCase().includes('xandered')) {
-        text[i].innerHTML = text[i].innerHTML.replace(/xandered/gi, 'Jaquaysed')
+    if (node.nodeValue.toLowerCase().includes('xandered')) {
+        node.nodeValue = node.nodeValue.replace(/xandered/gi, 'Jaquaysed')
     }
 }
