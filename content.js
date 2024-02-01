1d5595746b61516b5a5e4f593d857c21f11a558f
Riley Lum <rileylum@adam.com.au>
2024-02-01 14:15:05 +1030

    add div selector, add 'xandered' to match terms

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-const text = document.querySelectorAll('h1,h2,h3,h4,h5,p,li,td,caption,span,a')
+const text = document.querySelectorAll('h1,h2,h3,h4,h5,p,li,td,caption,span,a,div')
 
 for (let i = 0; i < text.length; i++) {
     if (text[i].innerHTML.toLowerCase().includes('xandering')) {
@@ -6,5 +6,8 @@ for (let i = 0; i < text.length; i++) {
     }
     if (text[i].innerHTML.toLowerCase().includes('jaquaying')) {
         text[i].innerHTML = text[i].innerHTML.replace(/jaquaying/gi, 'Jaquaysing')
+    }
+    if (text[i].innerHTML.toLowerCase().includes('xandered')) {
+        text[i].innerHTML = text[i].innerHTML.replace(/xandered/gi, 'Jaquaysed')
     }
 }
