cf78c1f11fd07b904809bbd680e7f00e5ef15400
Riley Lum <rileylum@adam.com.au>
2024-02-01 13:24:44 +1030

    change match string to reduce number of incorrect changes. like finding xander in Alexander (even if it is funny)

--- content.js
+++ content.js
@@ -1,7 +1,10 @@
 const text = document.querySelectorAll('h1,h2,h3,h4,h5,p,li,td,caption,span,a')
 
 for (let i = 0; i < text.length; i++) {
-    if (text[i].innerHTML.toLowerCase().includes('xander')) {
-        text[i].innerHTML = text[i].innerHTML.replace(/xander/gi, 'Jaquays')
+    if (text[i].innerHTML.toLowerCase().includes('xandering')) {
+        text[i].innerHTML = text[i].innerHTML.replace(/xandering/gi, 'Jaquaysing')
+    }
+    if (text[i].innerHTML.toLowerCase().includes('jaquaying')) {
+        text[i].innerHTML = text[i].innerHTML.replace(/jaquaying/gi, 'Jaquaysing')
     }
 }
